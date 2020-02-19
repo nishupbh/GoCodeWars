198f6a99f3bbcb32f0f8a3106e8c63c6b2f7035b
nishupbh <nishupbh@gmail.com>
2020-02-19 20:47:59 +0530

    new prob2

---
+++ HighestScoringWord.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func High(s string) string {
+	strSlice := strings.Split(strings.ToLower(s), " ")
+	var highScoringWord string
+	var highScore, temp int32
+	for _, value := range strSlice {
+		temp = 0
+		for _, strValue := range value {
+			temp += (strValue - 96)
+			fmt.Println(strValue - 96)
+		}
+		if highScore < temp {
+			highScore = temp
+			highScoringWord = value
+
+			fmt.Printf("High Score %v with  word %v \n", highScore, highScoringWord)
+		}
+	}
+	return highScoringWord
+}
+
+func main() {
+
+	fmt.Println(High("wiwwjvhcxf qhwpstkqv"))
+}
