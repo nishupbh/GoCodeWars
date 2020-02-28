d522e174cfd97cec5b8d4291e9493e8dc8bc033d
nishupbh <nishupbh@gmail.com>
2020-02-28 21:55:45 +0530

    twosum problem add

---
+++ twoSum.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+)
+
+//TwoSum will fetch the index of matching number
+func TwoSum(number []int, target int) [2]int {
+	for i := 0; i < len(number)-1; i++ {
+		for j := i + 1; j < len(number); j++ {
+			if number[i]+number[j] == target {
+				return [2]int{i, j}
+			}
+		}
+	}
+	return [2]int{}
+
+}
+
+func main() {
+	fmt.Println(TwoSum([]int{1, 2, 3, 4}, 4))
+}
